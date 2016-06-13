8bdfb2d1c549725b31154fce07d1fb5893fda8fa
Michael Angell <m.angell@culturekings.com.au>
2016-06-13 13:32:00 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var http = require('http');
+var url = require('url');
+var amqp = require('amqplib/callback_api');
+
+http.createServer(function(request, response) {
+    var headers = request.headers;
+    var method = request.method;
+    var requestUrl = url.parse(request.url, true);
+    var body = [];
+    request.on('error', function(err) {
+        console.error(err);
+    }).on('data', function(chunk) {
+        body.push(chunk);
+    }).on('end', function() {
+
+        body = Buffer.concat(body).toString();
+
+        amqp.connect(process.env.AMQP_SERVER, function(err, conn) {
+            conn.createChannel(function(err, ch) {
+
+                var q = (process.env.AMQP_PREFIX || 'queue') + requestUrl.pathname;
+
+                var message = {
+                    url : requestUrl,
+                    method : method,
+                    headers : headers,
+                    body : body,
+                };
+
+                ch.assertQueue(q, {durable: true});
+                ch.sendToQueue(q, new Buffer(JSON.stringify(message)));
+
+                response.writeHead(200, {'Content-Type': 'text/plain'});
+                response.end('ok');
+            });
+        });
+
+    });
+}).listen((process.env.PORT || 5000));
